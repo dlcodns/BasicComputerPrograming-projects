559d54910301c1391f23f9750f26d102565dc60f
dlcodns <redmond0412@gmail.com>
2023-01-06 14:39:19 +0900

    최대최소 구하기

--- Project1_정수합, 평균, 최대최소.c
+++ Project1_정수합, 평균, 최대최소.c
@@ -7,6 +7,8 @@ int main(void){
     int n;
     int add=0;
     float average;
+    int MAX=-999999;
+    int MIN=999999;
 
     printf("입력할 정수의 개수를 적으세요. >> ");
     scanf("%d",&n);
@@ -14,14 +16,20 @@ int main(void){
 
     printf("정수를 입력하세요.\n");
     for(int i=0;i<n;i++){
-        printf("%d번째 정수 : ",i+1);
+        printf("%d번째 정수 >> ",i+1);
         scanf("%d",&arr[i]);
-        add=+arr[i];
+        add+=arr[i];
+        if(arr[i]>MAX){
+            MAX=arr[i];
+        }
+        if(arr[i]<MIN){
+            MIN=arr[i];
+        }
     }
 
     average=(float)add/n;
 
-    printf("\n\n1. 합: %d\n2. 평균: %f\n",add,average);
+    printf("\n\n1. 합: %d\n2. 평균: %.1f\n3. 최대수: %d\n4. 최소수: %d\n",add,average,MAX,MIN);
 
     return 0;
 }
