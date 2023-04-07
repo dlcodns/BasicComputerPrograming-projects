fe59771a5125bb679a52a91b9f3164178aac363e
chaeun lee <107669084+dlcodns@users.noreply.github.com>
2023-04-07 14:09:56 +0900

    Update Quick.cpp

--- Quick.cpp
+++ Quick.cpp
@@ -9,48 +9,57 @@ void swap(int *a, int *b){      //서로 값을 바꾸는 함수
  *b = temp;
 }
 
-void split(int a[], int k, int m, int *i){    //퀵솔팅하는 과정 함수
-  int v = a[k];
-  int j=k+1; //j는 subarray a의 첫 번째 원소
-  *i = m; //i는 subarray a의 m(a의 마지막 원소)
+void split(int number[], int k, int m, int *i){    //퀵솔팅하는 과정 함수
+  int v = number[k];
+  int j=k+1; //j는 subarray number의 첫 번째 원소
+  *i = m; //i는 subarray number의 m(number의 마지막 원소)
   while( j <= *i ){
-    while(a[j] <= v && j<=*i)
+    while(number[j] <= v && j<=*i)
       j++; //만약 pivot값 보다 작다면 j++
-    while(a[*i] >= v && j<=*i)
+    while(number[*i] >= v && j<=*i)
       (*i)--; //만약 pivot값 보다 크다면 i--
     if (j< *i){
-      swap(&a[j], &a[*i]); //j과 i번째 index 값을 서로 바꾼다.
+      swap(&number[j], &number[*i]); //j과 i번째 index 값을 서로 바꾼다.
     } 
     }  
   if(*i!=k) {
-    a[k]=a[*i];
-    a[*i]=v;
-  } // == swap(a[k], a[i])
+    number[k]=number[*i];
+    number[*i]=v;
+  } // == swap(number[k], number[i])
 
 }
 
-void quick(int a[], int k, int m){    //퀵을 진행하는 함수
+void quick(int number[], int k, int m){    //퀵을 진행하는 함수
   int i=m; //i is pivot
   
   if(k<m){
-    split(a, k, m, &i);
+    split(number, k, m, &i);
          //pivot i를 기준으로 왼쪽값들
-    quick(a, k, i-1);
+    quick(number, k, i-1);
          //pivot i를 기준으로 오른쪽값들
-    quick(a, i+1, m);
+    quick(number, i+1, m);
   }
 }
 
-void fill(int a[], int num){    //빠진 수를 채우는 함수
+void fill(int number[], int num){    //빠진 수를 채우는 함수
+  int newNum[10]={0,};
+  for (int i=0;i<num;i++){
+    newNum[i]=number[i];
+  }
   for(int j=0;j<10;j++){
-    if(a[j]!=j){        //a[0]이 0이 아니면 0으로 만들고 나머지 뒤로 밀기
+    if(newNum[j]!=j){        //number[0]이 0이 아니면 0으로 만들고 나머지 뒤로 밀기
       for(int i=num;i>j;i--){
-        a[i] = a[i-1];
+        newNum[i] = newNum[i-1];
       }
-      a[j] = j;         //빠져있던 수 채우기
+      newNum[j] = j;         //빠져있던 수 채우기
       num++;            //수가 채워졌으니 배열의 수도 늘어나야 함
     }
+  } 
+  cout << "  Fill integer: ";  //채워진 수 출력
+  for(int i=0;i<10;i++){
+    cout << newNum[i] << " ";
   }
+  cout << "\n\n";  
 }
 
 int main(void) {
@@ -59,24 +68,24 @@ int main(void) {
     int num = 0;
     cout << "• 정렬할 1 ~ 9 사이의 서로 다른 정수들의 개수를 입력하세요. >> ";
     cin >> num;
-    if(num<=0 || num>10){
+    if(num<=0 || num>9){
       cout << "\n1 ~ 9 사이의 정수가 아닙니다. 다시 입력해주세요.\n\n" << endl;
       continue;
     }
-    int a[num];
+    int number[num];
     
     cout << "• 0 ~ 9 사이의 " << num << "개의 서로 다른 정수를 입력하세요. >> ";
     for(int i=0;i<num;i++){
-      cin >> a[i];
+      cin >> number[i];
     }
     
     int check = 0;        //check 초기화
     for(int i=0;i<num;i++){    //배열의 범위가 맞지 않을 경우 다시 시작
       for(int j=0;j<i;j++){
-        if((a[i]==a[j]) || (a[j]<0 || a[j]>9)){
+        if((number[i]==number[j]) || (number[j]<0 || number[j]>9)){
           cout << "\n0 ~ 9 사이의 서로 다른 정수가 아닙니다. 다시 입력해주세요.\n\n" << endl;
           check+=1;
-          j=i;    //j for문에서 나오기 위해 조건 불충분하게 만들기
+          j=num;    //j for문에서 나오기 위해 조건 불충분하게 만들기
           i=num;  //i for문에서 나오기 위해 조건 불충분하게 만들기
         }
       }
@@ -87,23 +96,18 @@ int main(void) {
 
     cout << "  First array: ";  //사용자가 입력한 배열 출력
     for(int i=0; i<num; i++){
-      cout << a[i] << " ";
+      cout << number[i] << " ";
     }
     cout << endl;
 
-    quick(a, 0, num-1);
+    quick(number, 0, num-1);
     cout << "  Quick sorting: ";  //배열을 퀵솔팅한 배열 출력
     for(int i=0;i<num;i++){
-      cout << a[i] << " ";
+      cout << number[i] << " ";
     }
     cout << endl;
 
-    fill(a, num);
-    cout << "  Fill integer: ";  //빠져있는 수 출
-    for(int i=0;i<10;i++){
-      cout << a[i] << " ";
-    }
-    cout << "\n\n";
+    fill(number, num);           //빈자리를 찾아 채워주는 함수
   }
   return 0;
 }
